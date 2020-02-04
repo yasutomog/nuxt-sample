0cdae302b4efb9cac950f13152899282fd41bd96
yasutomog <yasutomo.goto@gmail.com>
2020-02-04 19:47:21 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="item in items">
+      <nuxt-link :to="'careers/' + item.id">
+        <h2>
+          {{ item.name }}
+        </h2>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  async asyncData () {
+    const { data } = await axios.get(
+      'https://yasutomog.microcms.io/api/v1/careers',
+      {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+      }
+    )
+    return {
+      items: data.contents
+    }
+  }
+}
+</script>
