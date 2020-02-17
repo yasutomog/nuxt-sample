69f23acfeb525473af7365ac76a330f33e496985
yasutomog <yasutomo.goto@gmail.com>
2020-02-17 17:48:39 +0900

    netlify forms sample2

--- pages/index.vue
+++ pages/index.vue
@@ -7,17 +7,20 @@
         </h2>
       </nuxt-link>
     </div>
-    <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" placeholder="後藤"></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message" placeholder="テストメッセージ"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <client-only>
+      <form name="contact" method="POST" netlify>
+        <input type="hidden" name="form-name" value="contact">
+        <p>
+          <label>Your Name: <input type="text" name="name" placeholder="後藤"></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message" placeholder="テストメッセージ"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </client-only>
   </div>
 </template>
 <script>
