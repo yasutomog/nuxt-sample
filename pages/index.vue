4c3cef86c5723bc889075efbf71b8390097904d5
yasutomog <yasutomo.goto@gmail.com>
2020-02-17 18:20:15 +0900

    netlify forms sample5

--- pages/index.vue
+++ pages/index.vue
@@ -22,25 +22,15 @@
       <!--</form>-->
     <!--</client-only>-->
   <!--</div>-->
-  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+  <form
+          name="ask-question"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <input type="text" name="name">
+    <button>Submit</button>
   </form>
 </template>
 <script>
